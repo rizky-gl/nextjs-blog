25d7f7f11c1857789908392b5bccc179f9f27a1c
Rizky <123081575+rizky-gl@users.noreply.github.com>
2023-09-22 10:45:10 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,6 +38,10 @@ export default function Home() {
           </div>
         </div>
 
+        <div>
+            <p> Entah apa ini </p>
+        </div>
+
         <div className={styles.grid}>
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
