a4b29de96cb08391bb97d4761522e5f16a08fb9e
Rizky <123081575+rizky-gl@users.noreply.github.com>
2023-09-22 11:17:15 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,23 +19,6 @@ export default function Home() {
           <p>
             Haerin is the best
           </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
         </div>
 
         <div>
