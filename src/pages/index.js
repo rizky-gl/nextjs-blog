9045c1a6aa215ca7394a6156a1587e56bc91cc7c
rizky-gl <rizkyfatar14@gmail.com>
2023-09-22 13:12:23 +0700

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div>
+        <div className={styles.something}>
           <h3>Haerin Fun Fact:</h3>
           <li>
             Stage Name: Haerin (해린) Birth Name: Kang Haerin (강해린) English
