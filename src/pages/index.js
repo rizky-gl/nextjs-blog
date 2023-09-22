0d35bded611292d9da006040990b44a990b3aa34
rizky-gl <rizkyfatar14@gmail.com>
2023-09-22 12:13:12 +0700

    article fun fact

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             <h2>
-              First Article <span>-&gt;</span>
+              Haerin Fun-Fact <span>-&gt;</span>
             </h2>
             <p>
               Find in-depth information about Next.js features and&nbsp;API.
